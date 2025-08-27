c2f2b799101103d50ab8e39902e85d2f679d3efb
Harshita Sultanpure <129832894+HarshitaSultanpure@users.noreply.github.com>
2025-08-27 19:59:46 +0530

    Array

---
+++ leetcode/Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0, j=1;
+        int n = nums.size();
+
+        for(;j<n;j++)
+        {
+            if(nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
