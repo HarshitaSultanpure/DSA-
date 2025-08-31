bbafca4bc3d48854c0a44c9e75e3fcd3f37616f5
Harshita Sultanpure <129832894+HarshitaSultanpure@users.noreply.github.com>
2025-08-31 23:14:22 +0530

    Array

---
+++ Remove Element/Remove_Element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0,k=0;
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=val) nums[k++]=nums[i];
+        }
+        return k;
+    }
+};
